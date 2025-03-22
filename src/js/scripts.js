aadca74a6da511929bd8110730e10f7a32536755
Luca Xavier <britezlucaxavier@gmail.com>
2025-03-22 01:52:28 -0300

    Mejoras
    Nav bar animado
    Nav bar estilizado

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,12 +1,23 @@
-// Navbar sticky con desplazamiento suave
 window.addEventListener("scroll", function () {
-    var navbar = document.getElementById("navbar");
-    var sticky = navbar.offsetTop;
-  
-    if (window.pageYOffset > sticky) {
-      navbar.classList.add("sticky");
-    } else {
-      navbar.classList.remove("sticky");
+  var navbar = document.getElementById("navbar");
+  var links = navbar.getElementsByTagName("a"); // Selecciona todos los enlaces dentro del navbar
+  var sticky = navbar.offsetTop;
+
+  if (window.pageYOffset > sticky) {
+    navbar.classList.add("sticky");
+    navbar.style.backgroundColor = "white"; // Cambia el color de fondo a blanco
+
+    // Cambia el color de los enlaces a negro
+    for (var i = 0; i < links.length; i++) {
+      links[i].style.color = "black";
     }
-  });
-  
+  } else {
+    navbar.classList.remove("sticky");
+    navbar.style.backgroundColor = ""; // Restablece el color de fondo
+
+    // Restablece el color de los enlaces
+    for (var i = 0; i < links.length; i++) {
+      links[i].style.color = ""; // Restablece el color de los enlaces
+    }
+  }
+});
